9b03567f42806f6c26ffe646d12da5b357700f82
lee7670 <lee7670@gmail.com>
2015-02-24 14:22:39 -0500

    Renamed files and added citations for tutorials

--- VertPrintScript.py
+++
@@ -1,8 +0,0 @@
-import bpy
-print("start")
-print("verts")
-for i in range(len(bpy.data.objects["Crate"].data.vertices)):
-    print("point: "+str(i));
-    print('x: '+str(bpy.data.objects["Crate"].data.vertices[i].co.x))
-    print('y: '+str(bpy.data.objects["Crate"].data.vertices[i].co.y))
-    print('z: '+str(bpy.data.objects["Crate"].data.vertices[i].co.z))
